a040f2918e438794c1d2f413be330910976bb089
cucharachaa <69549694+cucharachaa@users.noreply.github.com>
2021-01-24 23:10:34 -0300

    Update pet.js

--- pet.js
+++ pet.js
@@ -4,22 +4,28 @@ pet.addEventListener("click",acariciar);
 var stop= document.getElementById("stop");
 stop.addEventListener("click",parar);
 
-function acariciar()
-{
+var cuchi = document.getElementById("cuchicuchi");
+function acariciar(){
     var monita = document.getElementById("monita");
     if (monita.src.match("mueve")) {
         monita.src = "monita_mueve.gif";
+        cuchi.play();
     } else {
         monita.src = "monita_mueve.gif";
+        cuchi.play();
     }
 }
-
-function parar()
-{
+function parar(){
 	var monita= document.getElementById("monita");
 	if (monita.src.match("monita")){
 		monita.src="monita.png";
+    cuchi.pause();
+    cuchi.currentTime = 0;
 	}else{
 		monita.src="monita.png";
+    cuchi.pause();
+    cuchi.currentTime = 0;
+
 	}
 }
+
